fbf6c8f130b994861722529256579053800dd936
Erika18Granados <erikav021899@gmail.com>
2020-09-22 20:01:50 -0500

    estructura LinkedList creada

---
+++ src/uaslp/engineering/labs/Main.java
@@ -0,0 +1,8 @@
+package uaslp.engineering.labs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/uaslp/engineering/labs/list/LinkedList.java
@@ -0,0 +1,30 @@
+package uaslp.engineering.labs.list;
+
+public class LinkedList {
+    private Node front;
+    private Node tail;
+
+    public void add(Student student) {
+
+    }
+
+    public void delete(Student student) {
+
+    }
+    public void delete(int index) {
+
+    }
+    public IteratorArrayList getIterator() {
+
+    }
+    public int size() {
+
+    }
+    public Student getAt(int index) {
+
+    }
+    public void insert(Student reference, Student newStudent) {
+
+    }
+
+}
